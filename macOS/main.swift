fcb5b4a6fdf3ab8ba24a1306b32c6db625f5b043
Angus McAloon <AMcAloon@lcs.on.ca>
2021-10-01 11:53:51 -0400

    User can view history.

--- macOS/main.swift
+++ macOS/main.swift
@@ -10,16 +10,47 @@ import Foundation
 // Make an emppty array of type prediction
 var history: [Prediction] = []
 
-// Generate a weather prediction
-let prediction = WeatherPredictionGenerator.getPrediction()
 
-// Add the prediction to the array
-history.append(prediction)
+while true {
+    print("Do you want another prediction? Y/N")
+    let UserInput = readLine()
 
-// What's in the array now?
-dump(history)
+    
+    switch UserInput {
+        
+    case "N":
+        print("Would you like to view your history? Y/N")
+        let UserInput = readLine()
+        
+        switch UserInput {
+        case "Y":
+            dump(history)
+        case "N":
+            break
+        case .none:
+            break
+        case .some(_):
+            print("Please input a valid choice")
+        }
+        
+    case "Y":
+        // Generate a weather prediction
+        let prediction = WeatherPredictionGenerator.getPrediction()
 
-// Show the prediction
-print("Current conditions are \(prediction.condition.description.lowercased()) with a temperature of \(String(format: "%.1f", arguments: [prediction.temperature])) °C.")
+        // Add the prediction to the array
+        history.append(prediction)
+        
+        // Show the prediction
+        print("Current conditions are \(prediction.condition.description.lowercased()) with a temperature of \(String(format: "%.1f", arguments: [prediction.temperature])) °C.")
 
-print("That's \(prediction.feel.lowercased())!")
+        print("That's \(prediction.feel.lowercased())!")
+    case .none:
+        break
+    case .some(_):
+        print("Please input a valid choice")
+    }
+
+}
+
+
+
