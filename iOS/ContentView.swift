8e02802db941c32f2e244e5853c52c01115e44ca
Angus McAloon <AMcAloon@lcs.on.ca>
2021-10-02 10:51:03 -0400

    iOS content accurately displays a new prediction, and the history of previous predictions

--- iOS/ContentView.swift
+++ iOS/ContentView.swift
@@ -10,20 +10,35 @@ import SwiftUI
 struct ContentView: View {
     
     var viewModel = WeatherViewModel()
+    @State var conditionDescription = ""
+    @State var temperature = 0.0
     
     var body: some View {
-        let prediction = viewModel.getPrediction()
-        Text("Current conditions are \(prediction.condition.description.lowercased()) with a temperature of \(String(format: "%.1f", arguments: [prediction.temperature])) °C.")
+
+//        var prediction = viewModel.getPrediction()
+        
+        Text("Current conditions are \(conditionDescription.lowercased()) with a temperature of \(String(format: "%.1f", arguments: [temperature])) °C.")
             .padding()
+        
+        Button(action: {
+            let prediction = viewModel.getPrediction()
+            conditionDescription = prediction.condition.description
+            temperature = prediction.temperature
+        }, label: {
+            
+            Text("New Prediction")
+            
+        })
+        
         List(viewModel.predictionHistory) { prediction in
-            Text("Current conditions are \(prediction.condition.description.lowercased()) with a temperature of \(String(format: "%.1f", arguments: [prediction.temperature])) °C.")
+            Text("Conditions were \(prediction.condition.description.lowercased()) with a temperature of \(String(format: "%.1f", arguments: [prediction.temperature])) °C.")
         }
     }
     
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView(viewModel: <#WeatherViewModel#>)
+//    }
+//}
